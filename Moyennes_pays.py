e844fe61ca829fd25ad332b40bba6ce2d6b1df72
Anne-Gabrielle Gibeili <anne-gabrielle.gibeili@etu.minesparis.psl.eu>
2023-10-31 16:04:49 +0100

    modif notebook

--- Moyennes_pays.py
+++ Moyennes_pays.py
@@ -2,7 +2,26 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt 
 
-df = pd.DataFrame(pd.read_excel('data2022.xls'))
+
+# +
+df=pd.DataFrame(pd.read_excel('data2022.xls'))
+
 df=df.set_index('Country name')
+# -
 
 group=df.groupby('Country name')
+group.head()
+
+country_name=df['Country name'].unique()
+country_name
+l=df['Country name'].value_counts()
+l['Lebanon']
+
+
+d={}
+df=df.fillna(0)
+df.dtypes
+
+
+for i in country_name:
+    d[name]=df[df['Country name']==name].sum(axis=1)/l[name]
