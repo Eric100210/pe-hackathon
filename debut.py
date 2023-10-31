dad89eaf2959ddd01c2be2c3c2219f61d532d953
Eric Kabis de Saint Chamas <eric.kabis_de_saint_chamas@etu.minesparis.psl.eu>
2023-10-31 15:37:58 +0100

    liste avec tous les pays séparés

--- debut.py
+++ debut.py
@@ -6,5 +6,17 @@ df.head()
 df2=df[df["year"]==2022]
 df2=df2.set_index("Country name")
 
+#Tableau avec simplement la colonne Life Ladder
 df3=df2[["Life Ladder"]]
 df3.head()
+
+#liste_pays contient la liste séparée des tableaux de chaque pays
+group=df.groupby("Country name")
+liste_pays=[]
+for i in df["Country name"].unique():
+    liste_pays=group.get_group(df["Country name"].unique()[i])
+print(liste_pays[0])
+
+
+
+
